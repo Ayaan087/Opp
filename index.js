2349ce1cb0ee83c56bb6405d5320763003461a6f
Ayaan087 <148577426+Ayaan087@users.noreply.github.com>
2023-10-28 16:00:57 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,49 @@
+import inquirer from "inquirer";
+class Student {
+    constructor(n) {
+        this.name = n;
+    }
+}
+class Person {
+    constructor() {
+        this.student = [];
+    }
+    addStudent(obj) {
+        this.student.push(obj);
+    }
+}
+const persons = new Person();
+const programmStart = async (persons) => {
+    do {
+        console.log("Wellcome Stranger");
+        const ans = await inquirer.prompt({
+            type: "list",
+            message: "Would you like to talk to yourself?",
+            name: "Select",
+            choices: ["yourself", "student"]
+        });
+        if (ans.Select == "yourself") {
+            console.log("I am talking to myself");
+            console.log("I am fine");
+        }
+        if (ans.Select == "Student") {
+            const ans = await inquirer.prompt({
+                type: "input",
+                message: "Which Student do you want to talk to?",
+                name: "Student"
+            });
+            const student = persons.student.find(val => val.name == ans.Student);
+            if (!student) {
+                const name = new Student(ans.Student);
+                persons.addStudent(name);
+                console.log(`Hello I am ${name}, and I am fine`);
+                console.log(persons.student);
+            }
+            if (student) {
+                console.log(`Hello I am ${student.name}, and I am fine............`);
+                console.log(persons.student);
+            }
+        }
+    } while (true);
+};
+programmStart(persons);
